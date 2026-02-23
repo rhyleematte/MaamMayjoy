4253e20f2006b25f8d497a4c44788adbfae19d0a
Gza <gerrylizarestrada@gmail.com>
2026-02-23 10:27:44 +0800

    updated feb 23

--- script.js
+++ script.js
@@ -1,31 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
-
     const modal = document.getElementById("videoModal");
     const closeBtn = document.getElementById("closeModal");
     const loomVideo = document.getElementById("loomVideo");
     const playVideoTab = document.getElementById("playVideoTab");
 
-    // Open Modal when the Play Video Tab is clicked
     playVideoTab.addEventListener("click", function () {
         modal.style.display = "flex";
-
-        // Set Loom embed link with autoplay
         loomVideo.src = "https://www.loom.com/embed/6d676da1428647b18d2b6767e796496c?autoplay=1";
     });
 
-    // Close Modal
     function closeModal() {
         modal.style.display = "none";
-        loomVideo.src = "";  // Stop video when closing
+        loomVideo.src = "";  
     }
 
     closeBtn.addEventListener("click", closeModal);
 
-    // Close when clicking outside modal
     window.addEventListener("click", function (event) {
         if (event.target === modal) {
             closeModal();
         }
     });
-
 });
